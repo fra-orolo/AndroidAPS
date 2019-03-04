75417f003e3d5fc5f1d2baa4e048bf279a12e7e3
Martin Ginkel <martin_ginkel@web.de>
2019-03-04 22:48:37 +0100

    Allow extended boluses with button, if not used for TBR

--- app/src/main/java/info/nightscout/androidaps/plugins/pump/insight/LocalInsightPlugin.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/pump/insight/LocalInsightPlugin.java
@@ -710,7 +710,9 @@ public class LocalInsightPlugin extends PluginBase implements PumpInterface, Con
     @Override
     public PumpEnactResult setExtendedBolus(Double insulin, Integer durationInMinutes) {
         PumpEnactResult result = cancelExtendedBolusOnly();
-        if (result.success) result = setExtendedBolusOnly(insulin, durationInMinutes);
+        if (result.success) {
+            result = setExtendedBolusOnly(insulin, durationInMinutes);
+        }
         try {
             fetchStatus();
             readHistory();
